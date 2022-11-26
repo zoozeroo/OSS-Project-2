7b9d8171b82f55ec514184dc64261fe48e5fa091
me <me@naver.com>
2022-11-26 22:38:56 +0900

    oss project2

---
+++ template.py
@@ -0,0 +1,97 @@
+#PLEASE WRITE THE GITHUB URL BELOW!
+#https://github.com/zoozeroo/OSS-Project-2.git
+
+import sys
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import *
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+def load_dataset(dataset_path):
+	data = pd.read_csv(dataset_path)
+	df = pd.DataFrame(data)
+
+	return df
+
+def dataset_stat(dataset_df):
+	n_feats = dataset_df.loc[:,dataset_df.columns!="target"].shape[1]
+	class01_df = dataset_df["target"].value_counts().to_frame()
+	n_class0 = class01_df.loc[0,:].item()
+	n_class1 = class01_df.loc[1,:].item()
+
+	return n_feats, n_class0, n_class1
+
+def split_dataset(dataset_df, testset_size):
+	x = dataset_df.drop(columns="target", axis=1)
+	y = dataset_df["target"]
+
+	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = testset_size)
+
+	return x_train, x_test, y_train, y_test
+
+def decision_tree_train_test(x_train, x_test, y_train, y_test):
+	dt_cls = DecisionTreeClassifier()
+	dt_cls.fit(x_train, y_train)
+
+	accuracy = accuracy_score(y_test, dt_cls.predict(x_test))
+	prec = precision_score(y_test, dt_cls.predict(x_test))
+	recall = recall_score(y_test, dt_cls.predict(x_test))
+
+	return accuracy, prec, recall
+
+def random_forest_train_test(x_train, x_test, y_train, y_test):
+	rf_cls = RandomForestClassifier()
+	rf_cls.fit(x_train, y_train)
+
+	accuracy = accuracy_score(y_test, rf_cls.predict(x_test))
+	prec = precision_score(y_test, rf_cls.predict(x_test))
+	recall = recall_score(y_test, rf_cls.predict(x_test))
+
+	return accuracy, prec, recall
+
+def svm_train_test(x_train, x_test, y_train, y_test):
+	svm_pipe = make_pipeline(StandardScaler(), SVC())
+	svm_pipe.fit(x_train, y_train)
+
+	accuracy = accuracy_score(y_test, svm_pipe.predict(x_test))
+	prec = precision_score(y_test, svm_pipe.predict(x_test))
+	recall = recall_score(y_test, svm_pipe.predict(x_test))
+
+	return accuracy, prec, recall
+
+def print_performances(acc, prec, recall):
+	#Do not modify this function!
+	print ("Accuracy: ", acc)
+	print ("Precision: ", prec)
+	print ("Recall: ", recall)
+
+if __name__ == '__main__':
+	#Do not modify the main script!
+	data_path = sys.argv[1]
+	data_df = load_dataset(data_path)
+
+	n_feats, n_class0, n_class1 = dataset_stat(data_df)
+	print ("Number of features: ", n_feats)
+	print ("Number of class 0 data entries: ", n_class0)
+	print ("Number of class 1 data entries: ", n_class1)
+
+	print ("\nSplitting the dataset with the test size of ", float(sys.argv[2]))
+	x_train, x_test, y_train, y_test = split_dataset(data_df, float(sys.argv[2]))
+
+	acc, prec, recall = decision_tree_train_test(x_train, x_test, y_train, y_test)
+	print ("\nDecision Tree Performances")
+	print_performances(acc, prec, recall)
+
+	acc, prec, recall = random_forest_train_test(x_train, x_test, y_train, y_test)
+	print ("\nRandom Forest Performances")
+	print_performances(acc, prec, recall)
+
+	acc, prec, recall = svm_train_test(x_train, x_test, y_train, y_test)
+	print ("\nSVM Performances")
+	print_performances(acc, prec, recall)
+
+
